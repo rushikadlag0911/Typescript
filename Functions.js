e98feb9e79279b4419866d92f4a7cac05aa054d3
Rushikesh Kadlag <62883177+rushikadlag0911@users.noreply.github.com>
2021-07-26 19:56:16 +0530

    typescript programs

--- Functions.js
+++ Functions.js
@@ -3,3 +3,11 @@ function fun(no1, no2) {
     console.log(no2);
 }
 fun(10, 11);
+function Addtion(number1, number2) {
+    var answer = 0;
+    answer = number1 + number2;
+    return answer;
+}
+var ret = 0;
+ret = Addtion(11);
+console.log("Additon is ", ret);
--- Functions.ts
+++ Functions.ts
@@ -3,3 +3,15 @@ function fun(no1 : number, no2 ? ) {
     console.log(no2);
 }
 fun(10,11);
+
+
+function Addtion(number1 : number , number2 ?) : number {
+    
+    let answer : number = 0;
+    answer = number1 + number2;
+    return answer;
+}
+
+var ret : number = 0 ;
+ret = Addtion(11);
+console.log("Additon is ", ret);
